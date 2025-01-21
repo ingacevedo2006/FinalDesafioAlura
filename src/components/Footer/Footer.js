7047ae4c0b907d43339bb15ce8c1742c997266bd
Ing. Acevedo <sa.projects@outlook.com>
2025-01-21 18:43:26 -0500

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <img src="/path-to-your-icon.png" alt="AluraFlix" />
+    </footer>
+  )
+}
+
+export default Footer
