65cc639eea9f571d0828f82de8afc59dda4fc119
Ing. Acevedo <sa.projects@outlook.com>
2025-01-21 21:56:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"; // Asegúrate de tener este archivo en `src`. Si no lo necesitas, omítelo.
+//import "./index.css"; // Asegúrate de tener este archivo en `src`. Si no lo necesitas, omítelo.
 import App from "./App";
 
 // Renderiza la aplicación principal en el div con el id "root" en `public/index.html`.
@@ -10,3 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
